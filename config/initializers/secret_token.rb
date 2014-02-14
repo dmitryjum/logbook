9d598ae642baf078b1e9247dcf142bd9ff648698
Dmitry Jum <xfiler@ymail.com>
2014-02-14 09:52:47 -0500

    First commit; rails new; changes to README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Logbook::Application.config.secret_key_base = 'b8d0c5ef6abc063e22f29b074811a9a47a6fda4135b1a3e22c4d0233c363880b927b538be8fac7136fa74abc16c8e35dfb76564072df393e60d59494a8343b71'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Logbook::Application.config.session_store :cookie_store, key: '_logbook_session'
