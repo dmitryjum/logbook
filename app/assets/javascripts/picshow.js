844a905d973619817b9c3e1e578be5110bc49c8d
Dmitry Jum <xfiler@ymail.com>
2014-07-04 04:25:38 -0400

    fixed avatar address, didn't work correct on heroku

--- app/assets/javascripts/picshow.js
+++ app/assets/javascripts/picshow.js
@@ -28,7 +28,7 @@ function postComment(pictureid, avatar) {
       url: "/pictures/" + pictureid + "/picture_comments",
       data: {picture_comment: {"comment": commentText}},
       success: function(response){
-        var newCommentLi = $('<li id="'+ response[0].id + '"><div class="reply_table"><div class="reply_wrapper"><img alt="' + response[1].first_name + '" src="/assets/' + avatar + '" class="reply_image" /><div class="reply_info"><p class="reply_author"><strong>' + response[1].first_name + ' ' + response[1].last_name + '</strong></p><p class="prely_text">' + response[0].comment + '</p><p class="reply_date">' + new Date(Date.parse(response[0].created_at)) + '<a class="delete_comment">X</a></p>');
+        var newCommentLi = $('<li id="'+ response[0].id + '"><div class="reply_table"><div class="reply_wrapper"><img alt="' + response[1].first_name + '" src="' + avatar + '" class="reply_image" /><div class="reply_info"><p class="reply_author"><strong>' + response[1].first_name + ' ' + response[1].last_name + '</strong></p><p class="prely_text">' + response[0].comment + '</p><p class="reply_date">' + new Date(Date.parse(response[0].created_at)) + '<a class="delete_comment">X</a></p>');
         commentUl.append(newCommentLi);
         commentTextField.val("");
       }
